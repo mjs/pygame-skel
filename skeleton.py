d505d95444d70f0aad2684ec1fe3a3ba60c7e3cf
Menno Finlay-Smits <inbox@menno.io>
2022-10-02 12:08:12 +1300

    First commit

---
+++ skeleton.py
@@ -0,0 +1,25 @@
+import sys,pygame
+pygame.init()
+
+screen = pygame.display.set_mode([640,480])
+pygame.key.set_repeat(5)
+
+x = 100
+
+while True:
+    screen.fill([255,255,255])
+    pygame.draw.rect(screen, (0, 0, 255), [x, 100, 50, 50], 5)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                x = x - 1
+
+            if event.key == pygame.K_RIGHT:
+                x = x + 1
+
+    pygame.display.flip()
